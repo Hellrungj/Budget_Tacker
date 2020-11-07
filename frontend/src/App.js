a0a71f29b05fe92c94d1ba36d98e7f751b2a2fea
hellrungj <johnjameshellrung@gmail.com>
2020-11-07 13:30:56 -0600

    Update the python version and react version while recreating the react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,25 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Layout from "./components/Layout";
-import Dashboard from "./components/Dashboard";
-import { CssBaseline } from "@material-ui/core/";
+import logo from './logo.svg';
+import './App.css';
 
-export default function App() {
+function App() {
   return (
-    <Router>
-      <div>
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <CssBaseline />
-        <Switch>
-          <Route path="/">
-            <Layout >
-              <Dashboard />
-            </Layout>
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
+
+export default App;
